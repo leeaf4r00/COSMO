7715be0332c71fe7697184805f9a5239b582e7ca
leeaf4r00 <96915495+leeaf4r00@users.noreply.github.com>
2023-03-28 08:17:44 -0300

    Create cosmo_v2.py

---
+++ cosmo_v2.py
@@ -0,0 +1,64 @@
+import requests
+import PySimpleGUI as sg
+import webbrowser
+
+# Define a janela da GUI
+sg.theme('Reddit')
+layout = [
+    [sg.Text("Escolha o tipo de pesquisa:"), sg.Radio('Código de barras', 'RADIO1',
+                                                      default=True, key="-EAN-"), sg.Radio('Descrição', 'RADIO1', key="-DESC-")],
+    [sg.Text("Insira o código de barras do produto:" if values["-EAN-"]
+             else "Insira a descrição do produto:"), sg.Input(key="-SEARCH-")],
+    [sg.Button("Buscar"), sg.Button("Sair")],
+    [sg.Text(size=(50, 3), key="-OUTPUT-")],
+    [sg.Text("Desenvolvido por Rafael Fernandes, Rurópolis-Pará"), sg.Text("  "),
+     sg.Button('Instagram', font=('Helvetica', 10, 'underline'), key='-INSTAGRAM-')]
+]
+
+window = sg.Window("Consulta de Produto", layout, size=(600, 300))
+
+# Loop para ler os eventos da janela
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == "Sair":
+        break
+
+    if values["-EAN-"]:
+        search_type = "gtins"
+        search_prompt = "Insira o código de barras do produto:"
+    else:
+        search_type = "products"
+        search_prompt = "Insira a descrição do produto:"
+
+    window.Element("-SEARCH-").Update(search_prompt)
+    search_term = values["-SEARCH-"]
+    url = f"https://api.cosmos.bluesoft.com.br/{search_type}/{search_term}.json"
+    headers = {"X-Cosmos-Token": "l5XSvKMx3dKizYndt_WBLg"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 404:
+        window["-OUTPUT-"].update("Produto não encontrado.")
+    elif response.status_code == 200:
+        data = response.json()
+        if search_type == "gtins":
+            descricao = data.get("description")
+            ncm = data.get("ncm")
+            codigo = data.get("gtin")
+        else:
+            descricao = data[0].get("description")
+            ncm = data[0].get("ncm")
+            codigo = data[0].get("gtin")
+        if not descricao or not ncm:
+            window["-OUTPUT-"].update("Informações do produto não disponíveis.")
+        else:
+            window["-OUTPUT-"].update(
+                f"Código EAN: {codigo}\nDescrição: {descricao}\nNCM: {ncm}")
+    else:
+        window["-OUTPUT-"].update(f"Erro na consulta: {response.status_code}")
+
+    if event == '-INSTAGRAM-':
+        webbrowser.open_new_tab(
+            'https://www.instagram.com/RAFAELMOREIRAFERNANDES/')
+
+window.close()
